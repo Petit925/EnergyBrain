3d862d7b90fb37c1441026ed5bd75453df80e494
Petit925 <galabitskiy@gmail.com>
2025-04-06 19:38:22 +0300

    Update main_logic.py

--- main_logic.py
+++ main_logic.py
@@ -2,7 +2,7 @@ import fitz  # PyMuPDF
 import tiktoken
 import streamlit as st
 from openai import OpenAI
-from pinecone import Pinecone
+import pinecone
 
 # Ініціалізація OpenAI та Pinecone
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
